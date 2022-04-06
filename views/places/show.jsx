2d2d8860c05098974bb119a6de2c8b6542c60f9f
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-04-06 07:52:06 -0700

    comment is showing up on page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,6 +3,26 @@ const Def = require('../default')
 
 
 function show (data) {
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className='border'>
+            <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <strong>- {c.author}</strong>
+            </h3>
+            <h4>Rating: {c.stars} </h4>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
           <main>
@@ -14,8 +34,7 @@ function show (data) {
                 </h3>
               </div>
               <div className="col-sm-6">
-              <h2>Rating</h2>
-                <p>Currently Unrated</p>
+            
                 <h2>Description</h2>
                 <h3>
                   {data.place.showEstablished()}
@@ -35,7 +54,7 @@ function show (data) {
 
             </div>
             <h2>Comments</h2>
-                <p>no comments</p>
+              {comments}
           </main>
         </Def>
     )
